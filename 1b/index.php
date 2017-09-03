ff0f39df37d5da2fb46d5b203639009dad829d7c
Saurav Sahu <sauravsahu.12@live.com>
2017-09-03 15:48:43 +0530

    Remove unnecessary files and update file paths.

--- 1b/index.php
+++ 1b/index.php
@@ -7,7 +7,7 @@
   <head><title><?php echo $title?></title></head>
   <body>
     <h1><?php echo $title?></h1>
-    <form action="/ip-lab/html/1b/runCommand.php" method='post'>
+    <form action="/ip-lab/1b/runCommand.php" method='post'>
            <label>Enter a valid UNIX Command <input type='text' name="command" id="command"></label>
           <button type="submit">Execute</button>
     </form>
--- 2a/index.php
+++ 2a/index.php
@@ -3,7 +3,7 @@
   <body>
     <div>
         <h1>What's your name?</h1>
-         <form action="/ip-lab/html/2a/greet.php" method='post'> 
+         <form action="/ip-lab/2a/greet.php" method='post'> 
             <input type='text' name="userName" id="userName"></label>
             <button type="submit">Submit</button>
         </form>

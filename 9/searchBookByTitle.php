043bf2510919d38d169525d52169a8c979e355b7
Saurav Sahu <sauravsahu.12@live.com>
2017-09-08 23:38:02 +0530

    Add PHP script to search for a book by title and show filtered results.

---
+++ 9/searchBookByTitle.php
@@ -0,0 +1,59 @@
+<?php
+    include($_SERVER['DOCUMENT_ROOT'].'/ip-lab/php-includes/book.php');
+
+    $title = 'Search for a Book';
+
+    $servername = 'localhost';
+    $username = trim(`cat ../secrets/username`);
+    $password = trim(`cat ../secrets/password`);
+    $db = 'books';
+    $conn = new mysqli($servername, $username, $password, $db);
+
+    if ($conn->connect_error) die ("Connection failed: " . $conn->connect_error);
+    else
+    {
+        $orig = $_REQUEST['title'];
+        $title = strtolower($orig);
+        $sql = "SELECT * FROM `book` WHERE LOWER(title) LIKE '%$title%'";
+        $rows = $conn->query($sql);
+        $books = Book::parseSqlRows($rows);
+        if (count($books) == 0) die("No results found for '$orig'.");
+    }
+
+    function bookToTableRow($book)
+    {
+        return "
+        <tr>
+            <td>$book->ISBN</td>
+            <td>$book->Title</td>
+            <td>$book->Author</td>
+            <td>$book->Edition</td>
+            <td>$book->Publication</td>
+        </tr>";
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title><?php echo $title?></title>
+    <link rel='stylesheet' href='/ip-lab/9/styles.css'>
+</head>
+
+<body>
+    <h1>Search Results</h1>
+    <table>
+        <tr>
+            <th>ISBN</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Edition</th>
+            <th>Publication</th>
+        </tr>
+        <?php 
+            foreach($books as $b)
+                echo bookToTableRow($b);
+        ?>
+    </table>
+</body>
+</html>
